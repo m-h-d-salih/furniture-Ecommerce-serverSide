b3c93f7b33b6b5f916d8d0953b0c4fb1666486b6
m-h-d-salih <salihsha656@gmail.com>
2024-09-28 19:55:12 +0530

    product delete successsfully

--- src/controllers/adminSide/adminProductControll/adminProductControll.js
+++ src/controllers/adminSide/adminProductControll/adminProductControll.js
@@ -35,6 +35,21 @@ export const addProduct=async(req,res)=>{
     }
   }
 }
+//delete product
+
+export const deleteProduct=async(req,res)=>{
+    try{
+        const productId=req.params.id;
+        if(!mongoose.Types.ObjectId.isValid(productId)) return res.status(400).json({success:false,message:`invalid product id`})
+        const deletedproduct=await Products.findByIdAndDelete(productId)
+        if(!deletedproduct) return res.status(404).json({success:false,message:`product not found`})
+        res.status(200).json({success:true,message:`product deleted successfully`,data:deletedproduct})
+        
+
+    }catch(error){
+    return res.status(500).json({success:false,message:`internal server error ${error.message}`})
+}
+}
 
 
 //update product
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import checkAuth from "../middleware/checkAuth.js";
 import { blockAndUnblockUser, getAllUsers, getUserById } from "../controllers/adminSide/adminUserListController/adminUserListController.js";
-import { addProduct, updateProduct } from "../controllers/adminSide/adminProductControll/adminProductControll.js";
+import { addProduct, deleteProduct, updateProduct } from "../controllers/adminSide/adminProductControll/adminProductControll.js";
 
 const adminRouter=express.Router();
 
@@ -12,6 +12,7 @@ adminRouter.post('/users/:id',checkAuth,blockAndUnblockUser)
 
 adminRouter.post('/products',checkAuth,addProduct)
 adminRouter.post('/products/:id',checkAuth,updateProduct)
+adminRouter.delete('/products/:id',checkAuth,deleteProduct)
 
 
 export default adminRouter;
